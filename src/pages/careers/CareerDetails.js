852ff2d70187cafd63dcbce5188b7097ab9a51a5
Allan Kiche <tle2819@students.mu.ac.ke>
2023-01-10 12:44:18 +0300

    careerdetails2

--- src/pages/careers/CareerDetails.js
+++ src/pages/careers/CareerDetails.js
@@ -1,12 +1,30 @@
-import { useParams } from "react-router-dom"
+import { useLoaderData, useParams } from "react-router-dom"
 
 export default function CareerDetails() {
 
     const {id} = useParams()
+    const career = useLoaderData()
 
   return (
     <div className="career-details">
-        <h2>{ id }</h2>
+        <h2> Career Details for {career.title}</h2>
+        <p>Starting Salary: {career.salary}</p>
+        <p>Location: {career.location}</p>
+
+        <div className="details">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae voluptates doloribus, eaque est debitis, voluptatem natus ducimus expedita, ipsa ab laborum repellat ullam officiis dignissimos quia facere id odio architecto tempora modi! Sunt, vel eveniet ullam molestiae quae, architecto voluptatibus sed dolorum ipsam voluptates magni ut cum consequatur ratione omnis eaque adipisci blanditiis quibusdam. Labore velit molestias quasi aperiam illo!</p>
+        </div>
     </div>
   )
+}
+
+
+//loader function
+export const careerDetailsLoader = async ({params}) => {
+    const {id} = params
+
+    const res = await fetch("http://localhost:4000/careers" + id)
+
+    return res.json()
+
 }
