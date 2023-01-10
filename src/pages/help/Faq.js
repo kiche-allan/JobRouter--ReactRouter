212da0be6e51ee377871f50cb977600c35b72267
Allan Kiche <tle2819@students.mu.ac.ke>
2023-01-10 06:35:58 +0300

    help

---
+++ src/pages/help/Faq.js
@@ -0,0 +1,34 @@
+
+
+export default function Faq() {
+  return (
+    <div className="faq">
+        <h3>Frequently Asked Questions</h3>
+
+        <div className="question">
+            <p><strong>Lorem inpsum dolor sit amet</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam eos error, nam incidunt consequuntur esse. Beatae repellendus iure explicabo fugiat officiis labore minus, deleniti reprehenderit repellat laborum officia dolores velit? Quae repellendus labore atque iusto, non illum reprehenderit aliquam ipsa, mollitia recusandae possimus, earum architecto ipsum numquam doloribus. Soluta, in.</p>
+        </div>
+
+        <div className="question">
+            <p><strong>Lorem inpsum dolor sit amet</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam eos error, nam incidunt consequuntur esse. Beatae repellendus iure explicabo fugiat officiis labore minus, deleniti reprehenderit repellat laborum officia dolores velit? Quae repellendus labore atque iusto, non illum reprehenderit aliquam ipsa, mollitia recusandae possimus, earum architecto ipsum numquam doloribus. Soluta, in.</p>
+        </div>
+
+        <div className="question">
+            <p><strong>Lorem inpsum dolor sit amet</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam eos error, nam incidunt consequuntur esse. Beatae repellendus iure explicabo fugiat officiis labore minus, deleniti reprehenderit repellat laborum officia dolores velit? Quae repellendus labore atque iusto, non illum reprehenderit aliquam ipsa, mollitia recusandae possimus, earum architecto ipsum numquam doloribus. Soluta, in.</p>
+        </div>
+
+        <div className="question">
+            <p><strong>Lorem inpsum dolor sit amet</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam eos error, nam incidunt consequuntur esse. Beatae repellendus iure explicabo fugiat officiis labore minus, deleniti reprehenderit repellat laborum officia dolores velit? Quae repellendus labore atque iusto, non illum reprehenderit aliquam ipsa, mollitia recusandae possimus, earum architecto ipsum numquam doloribus. Soluta, in.</p>
+        </div>
+
+        <div className="question">
+            <p><strong>Lorem inpsum dolor sit amet</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam eos error, nam incidunt consequuntur esse. Beatae repellendus iure explicabo fugiat officiis labore minus, deleniti reprehenderit repellat laborum officia dolores velit? Quae repellendus labore atque iusto, non illum reprehenderit aliquam ipsa, mollitia recusandae possimus, earum architecto ipsum numquam doloribus. Soluta, in.</p>
+        </div>
+    </div>
+  )
+}
